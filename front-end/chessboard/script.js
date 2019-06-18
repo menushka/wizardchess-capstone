1494a75bf3d43b0b7443c4cec10f8d740f23c3a6
charles <charles.marquez@hotmail.com>
2019-06-17 20:30:52 -0400

    implementing necessary socket emits

--- front-end/chessboard/script.js
+++ front-end/chessboard/script.js
@@ -4,6 +4,21 @@ var game = new Chess()
 var squareToHighlight = null
 var squareClass = 'square-55d63'
 
+const socket = io('http://localhost:8000', { query: { client:"frontend" } });
+
+var onevent = socket.onevent;
+socket.onevent = function (packet) {
+var args = packet.data || [];
+onevent.call (this, packet);    // original call
+packet.data = ["*"].concat(args);
+onevent.call(this, packet);      // additional call to catch-all
+};
+
+socket.on("*",function(event,data) {
+console.log(event);
+console.log(data);
+});
+
 function removeHighlights (color) {
   $board.find('.' + squareClass)
     .removeClass('highlight-' + color)
@@ -61,12 +76,31 @@ function onDrop (source, target) {
   if (move === null) return 'snapback'
 
   // highlight white's move
+
+  console.log(move);
+
   removeHighlights('white')
   $board.find('.square-' + source).addClass('highlight-white')
   $board.find('.square-' + target).addClass('highlight-white')
 
+  ChessPiece = {
+    piece: move.piece,
+    colour: move.colour
+  }
+  
+  BoardLocation = {
+    from: move.from,
+    to: move.to
+  }
+
+  socket.emit('chessPieceM1oved', {
+    piece: move.piece,
+    colour: move.colour,
+    location: move.to
+  });
+
   // make random move for black
-//   window.setTimeout(makeRandomMove, 250)
+  window.setTimeout(makeRandomMove, 250)
 }
 
 function onMoveEnd () {
@@ -147,5 +181,7 @@ var config = {
   onMoveEnd: onMoveEnd,
   onSnapEnd: onSnapEnd
 }
+
+socket.emit('startGame',  { type: 1});
 
 board = Chessboard('board1', config)
